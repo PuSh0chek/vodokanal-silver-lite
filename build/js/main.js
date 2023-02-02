8c10dd67ab05c7824354d1f95e1db8ed2c5e337b
Alex <87208475+PuSh0chek@users.noreply.github.com>
2023-02-02 11:47:40 +0300

    0.378 Вывод основных popup'ов

--- build/js/main.js
+++ build/js/main.js
@@ -22,6 +22,15 @@ const counterLicen = document.querySelector('.about-programm__count-licence-of-p
 const countLicen = 0;
 const versionContainer = document.querySelector('.about-programm__number-version-of-programm');
 const version = 0.376;
+const adminMainMenu = document.querySelector('.admin__container');
+const workersPopup = document.querySelector('.admin__structure-of-the-enterprise');
+const groupAccessPopup = document.querySelector('.admin__group-access');
+const structureFolder = document.querySelector('.admin__structure-folder');
+const dictionaries = document.querySelector('.admin__dictionaries');
+const fileStorage = document.querySelector('.admin__file-storage');
+const systemCondiguration = document.querySelector('.admin__system-configuration');
+const protocols = document.querySelector('.admin__protocols');
+const license = document.querySelector('.admin__license');
 
 // Универсальные функции //
 const getRequiredWindow = (showElement, removeElementOne, removeElementTwo, removeElementThree, displayShow, displayRemove) => {
@@ -50,27 +59,115 @@ buttonArchive.addEventListener('click', () => {
 buttonAdmin.addEventListener('click', () => {
   getRequiredWindow(adminPage, archivePage, settingsPage, aboutProgrammPage, displayGrid, displayNone);
 });
+
+// Блок кода появления popup's admin //
+Array.from(adminMainMenu.children).forEach(item => {
+  item.addEventListener('click', () => {
+    if (item === Array.from(adminMainMenu.children)[0]) {
+      workersPopup.style = 'display: block';
+      groupAccessPopup.style = 'display: none';
+      structureFolder.style = 'display: none';
+      dictionaries.style = 'display: none';
+      fileStorage.style = 'display: none';
+      systemCondiguration.style = 'display: none';
+      protocols.style = 'display: none';
+      license.style = 'display: none';
+    } else if (item === Array.from(adminMainMenu.children)[1]) {
+      workersPopup.style = 'display: none';
+      groupAccessPopup.style = 'display: block';
+      structureFolder.style = 'display: none';
+      dictionaries.style = 'display: none';
+      fileStorage.style = 'display: none';
+      systemCondiguration.style = 'display: none';
+      protocols.style = 'display: none';
+      license.style = 'display: none';
+    } else if (item === Array.from(adminMainMenu.children)[2]) {
+      workersPopup.style = 'display: none';
+      groupAccessPopup.style = 'display: none';
+      structureFolder.style = 'display: block';
+      dictionaries.style = 'display: none';
+      fileStorage.style = 'display: none';
+      systemCondiguration.style = 'display: none';
+      protocols.style = 'display: none';
+      license.style = 'display: none';
+    } else if (item === Array.from(adminMainMenu.children)[3]) {
+      workersPopup.style = 'display: none';
+      groupAccessPopup.style = 'display: none';
+      structureFolder.style = 'display: none';
+      dictionaries.style = 'display: block';
+      fileStorage.style = 'display: none';
+      systemCondiguration.style = 'display: none';
+      protocols.style = 'display: none';
+      license.style = 'display: none';
+    } else if (item === Array.from(adminMainMenu.children)[4]) {
+      workersPopup.style = 'display: none';
+      groupAccessPopup.style = 'display: none';
+      structureFolder.style = 'display: none';
+      dictionaries.style = 'display: none';
+      fileStorage.style = 'display: block';
+      systemCondiguration.style = 'display: none';
+      protocols.style = 'display: none';
+      license.style = 'display: none';
+    } else if (item === Array.from(adminMainMenu.children)[5]) {
+      workersPopup.style = 'display: none';
+      groupAccessPopup.style = 'display: none';
+      structureFolder.style = 'display: none';
+      dictionaries.style = 'display: none';
+      fileStorage.style = 'display: none';
+      systemCondiguration.style = 'display: block';
+      protocols.style = 'display: none';
+      license.style = 'display: none';
+    } else if (item === Array.from(adminMainMenu.children)[6]) {
+      workersPopup.style = 'display: block';
+      groupAccessPopup.style = 'display: none';
+      structureFolder.style = 'display: none';
+      dictionaries.style = 'display: none';
+      fileStorage.style = 'display: none';
+      systemCondiguration.style = 'display: none';
+      protocols.style = 'display: block';
+      license.style = 'display: none';
+    } else if (item === Array.from(adminMainMenu.children)[7]) {
+      workersPopup.style = 'display: none';
+      groupAccessPopup.style = 'display: none';
+      structureFolder.style = 'display: none';
+      dictionaries.style = 'display: none';
+      fileStorage.style = 'display: none';
+      systemCondiguration.style = 'display: none';
+      protocols.style = 'display: none';
+      license.style = 'display: block';
+    }
+  });
+});
 // Вызов окна настроек //
 buttonSettings.addEventListener('click', () => {
   getRequiredWindow(settingsPage, adminPage, archivePage, aboutProgrammPage, displayGrid, displayNone);
 });
+// Вывод popup's settings //
+const settingsUser = document.querySelector('.settings__popup-of-user-settings');
+const generalisSettings = document.querySelector('.settings__popup-of-generalis-settings');
+const scanningSettings = document.querySelector('.settings__popup-of-scanning-settings');
+const buttonSettingsUser = document.querySelector('.settings__user-button');
+const buttonSettingsGeneralis = document.querySelector('.settings__generalis-button');
+const buttonScanning = document.querySelector('.settings__scanning');
+buttonSettingsUser.addEventListener('click', () => {
+  settingsUser.style = 'display: block';
+  generalisSettings.style = 'display: none';
+  scanningSettings.style = 'display: none';
+});
+buttonSettingsGeneralis.addEventListener('click', () => {
+  settingsUser.style = 'display: none';
+  generalisSettings.style = 'display: block';
+  scanningSettings.style = 'display: none';
+});
+buttonScanning.addEventListener('click', () => {
+  settingsUser.style = 'display: none';
+  generalisSettings.style = 'display: none';
+  scanningSettings.style = 'display: block';
+});
+
 // Вызов окна о программе //
 buttonAbout.addEventListener('click', () => {
   getRequiredWindow(aboutProgrammPage, adminPage, settingsPage, archivePage, displayGrid, displayNone);
-});
-
-// Блок кода появления popup's admin //
-const structure = document.querySelector('.admin__structure-of-the-enterprise');
-const structureWorkingEmloyees = document.querySelector('.admin__working-employees');
-const structureSettingsUsers = document.querySelector('.admin__settings-users');
-const buttonsAdminMainMenu = document.querySelectorAll('.admin__button-of-container');
-const adminMainMenu = document.querySelector('.admin__container');
-const workersPopup = document.querySelector('.admin__structure-of-the-enterprise');
-Array.from(adminMainMenu.children).forEach(item => {
-  item.addEventListener('click', () => {
-    console.log(1);
-    workersPopup.style = 'display: block';
-  });
 });
 // Блок вызова функций //
 getCountLicen();
