dca04fecfb77eb94ee39a278681f9cb084adccf2
Alex <87208475+PuSh0chek@users.noreply.github.com>
2023-03-15 15:45:19 +0300

    Стили для popup удаление папок

--- source/js/views/delete-folder-of-popup.js
+++ source/js/views/delete-folder-of-popup.js
@@ -1,6 +1,6 @@
 export const getHtmlDeleteFolderOfPopup = () => `<h3 class="work-width-the-file__title-popup-of-delete-folder">Удаление папки</h3>
         <input class="work-width-the-file__input-popup-of-delete-folder" type="text" placeholder="Введите id папки">
-        <div class="">
+        <div class="work-width-the-file__wrapper-button-popup-of-delete-folder">
           <button class="work-width-the-file__button-popup-of-delete-folder" id="dutton-of-delete-folder">Удалить</button>
           <button class="work-width-the-file__button-popup-of-delete-folder">Назад</button>
         </div>`;
